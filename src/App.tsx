1f2ee8ee5bbac605f497157181b3067542514318
Ефимычева Ксения <ef.ks.al@mail.ru>
2023-09-16 11:54:41 +0400

    fix after rebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './styles/reset.css'
+import './styles/reset.css';
+import './App.css';
 import { ToastContainer } from 'react-toastify';
 import { Header } from './shared/header/header';
 import Catalog from './pages/catalog/catalog';
 import 'react-toastify/dist/ReactToastify.css';
 import ShoppingCart from './pages/shoppingCart/page';
+import Home from './pages/home/home';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Header />
         <div className="content-container">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/shoppingCart" element={<ShoppingCart />} />
             <Route path="/catalog" element={<Catalog />} />
           </Routes>
--- src/shared/layout/layout.tsx
+++ src/shared/layout/layout.tsx
@@ -1,6 +1,5 @@
 import React, { PropsWithChildren } from 'react';
 import './layout.css'
-import { Header } from '@/shared/header/header';
 
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
