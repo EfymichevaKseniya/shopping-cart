4c0c6f440b7471b402a7a358fd03f121ba1113ae
Ефимычева Ксения <ef.ks.al@mail.ru>
2023-11-13 16:07:18 +0400

    add animation for adding in cart

--- src/entities/catalogCard/catalogCard.tsx
+++ src/entities/catalogCard/catalogCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement, useState } from 'react';
 import './catalogCard.css'
 import Button from '@/shared/button/button';
 import { addToCart } from '@/store/reducers/shoppingCartSlice';
@@ -11,15 +11,27 @@ const CatalogCard: React.FC<ProductItem> = (product) => {
   const { cartItems } = useAppSelector(state => state.shoppingCart)
   const dispatch = useAppDispatch();
   const { id, image, name, price } = product;
+  const [addedToCart, setAddedToCart] = useState(false)
 
   const handleAddToCart = () => {
     dispatch(addToCart(product));
+    setAddedToCart(true);
+
+    setTimeout(() => {
+      setAddedToCart(false);
+    }, 1000)
   };
 
   const foundedInCartById = cartItems.find(item => item.id === id)
+  const clonedElement = React.cloneElement(
+    <figure className={`card__image-wrapper ${addedToCart ? 'fly' : ''}`}>
+      <img src={image} alt={name} />
+    </figure>
+  )
 
   return (
     <div className='card__wrapper' key={id}>
+      {addedToCart && clonedElement}
       <figure className='card__image'>
         <img src={image} alt={name} />
       </figure>
