5ec7fd7780d02547cb89d69aec03042ad5ffa1df
Ксения Ефимычева <ef.ks.al@mail.ru>
2023-11-13 15:32:43 +0400

    add fontawesome icons

--- src/shared/header/header.tsx
+++ src/shared/header/header.tsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { useAppSelector } from '@/store/hooks';
 import { RootState } from '@/store/store';
 import routes from '@/navigator/routes.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
@@ -18,9 +20,10 @@ export const Header: React.FC = () => {
       />
       <div className='cart__button'>
         <Button
-          title='Корзина'
           onClick={() => navigate(routes.cart)}
-        />
+        >
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </Button>
         <span className='cart__items-count'>{cartItems.length || 0}</span>
       </div>
     </div>
